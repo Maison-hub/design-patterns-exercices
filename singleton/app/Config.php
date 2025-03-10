88a94bef19f5f2b1dc8f5dfac6b051b1ddb5378a
Antoine <arotakerpro@gmail.com>
2025-03-10 17:13:08 +0100

    :sparkles: add singleton pattern

--- singleton/app/Config.php
+++ singleton/app/Config.php
@@ -3,3 +3,27 @@
 # TODO: Créer une classe Config en Singleton
 
 namespace App;
+
+class Config
+{
+    private static $instance = null;
+    private $data;
+
+    private function __construct()
+    {
+        $this->data = require dirname(__DIR__) . '/config/config.php';
+    }
+
+    public static function getInstance(): Config
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new Config();
+        }
+        return self::$instance;
+    }
+
+    public function get(string $key)
+    {
+        return $this->data[$key] ?? null;
+    }
+}
--- singleton/public/index.php
+++ singleton/public/index.php
@@ -5,3 +5,10 @@ require('../vendor/autoload.php');
 # TODO: Récuperer une instance de Config
 # Afficher une valeur contenu dans config.php
 # Récupérer une seconde instance de Config et vérifié que les deux instances sont identiques
+
+$config = App\Config::getInstance();
+$config2 = App\Config::getInstance();
+
+echo $config->get('apiKey') . PHP_EOL;
+echo "<br>";
+echo $config2->get('apiKey') . PHP_EOL;
